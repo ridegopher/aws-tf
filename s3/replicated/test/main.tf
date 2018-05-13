391241a693fcc4b3bb5cabea5070e81d491a18fa
Chris Dornsife <chris@dornsife.com>
2018-05-13 11:58:06 -0500

    fix provider and iam policy

--- s3/replicated/test/main.tf
+++ s3/replicated/test/main.tf
@@ -1,7 +1,9 @@
 
-
+module "state" {
+  source = "../../../.common/state"
+}
 
 module "s3_replicated_test" {
   source = "../"
-  bucket_name = "s3_replicated_test-cdn-999"
+  bucket_name = "s3-replicated-test-${module.state.account_id}"
 }
