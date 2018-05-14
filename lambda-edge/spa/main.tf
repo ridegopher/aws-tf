fff321e5afe7102ab7b19261c5b40b57f52acdb5
Chris Dornsife <chris@dornsife.com>
2018-05-13 20:36:53 -0500

    edge lambda for single page apps

---
+++ lambda-edge/spa/main.tf
@@ -0,0 +1,58 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "lambda_name" {
+  default = "edge-spa"
+}
+
+provider "aws" {
+  region = "${var.region}"
+}
+
+data "aws_iam_policy_document" "lambda" {
+  provider = "aws"
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = [
+        "lambda.amazonaws.com",
+        "edgelambda.amazonaws.com"
+      ]
+    }
+  }
+}
+
+data "archive_file" "lambda" {
+  type = "zip"
+  output_path = "${path.module}/.zip/lambda.zip"
+  source_dir = "${path.module}/lambda"
+}
+
+resource "aws_iam_role" "lambda" {
+  provider = "aws"
+  name = "${var.lambda_name}-role"
+  assume_role_policy = "${data.aws_iam_policy_document.lambda.json}"
+}
+
+resource "aws_lambda_function" "lambda" {
+  provider = "aws"
+  function_name = "${var.lambda_name}"
+  filename = "${data.archive_file.lambda.output_path}"
+  source_code_hash = "${data.archive_file.lambda.output_base64sha256}"
+  role = "${aws_iam_role.lambda.arn}"
+  runtime = "nodejs6.10"
+  handler = "index.handler"
+  memory_size = 128
+  timeout = 3
+  publish = true
+}
+
+output "arn" {
+  value = "${aws_lambda_function.lambda.arn}"
+}
+
+output "version" {
+  value = "${aws_lambda_function.lambda.version}"
+}
